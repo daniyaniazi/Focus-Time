b735f0bd7ad39ee55f526b1fe7c558923973d12a
Daniya Niazi <farihadania@hotmail.com>
2021-07-23 22:11:59 +0500

    A simple Text component

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Focus } from "./src/features/focus/Focus";
 
+// this file exports App
 export default function App() {
-  return <View style={styles.container}>Hello word</View>;
+  const [focusSubject, setFocusSubject] = useState(null);
+  return (
+    <View style={styles.container}>
+      {focusSubject ? (
+        <Text>Here is where i am going to build a time</Text>
+      ) : (
+        <Focus />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#225225",
     alignItems: "center",
     justifyContent: "center"
   }
---
+++ src/features/focus/Focus.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+// named export
+export const Focus = () => {
+  return (
+    <View>
+      <Text>What would you like to focus on ?</Text>
+    </View>
+  );
+};
